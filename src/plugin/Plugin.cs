583f08428666965a4b93f8d7e93a21b95a89df96
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2023-02-07 19:00:03 -0500

    Initial Commit

--- src/plugin/Plugin.cs
+++ src/plugin/Plugin.cs
@@ -1,14 +1,47 @@
 ﻿using BepInEx;
+using UnityEngine;
 
-namespace ExamplePlugin
+namespace PipeJukeNerf
 {
-    [BepInPlugin("com.example.examplemod", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin("com.coder23848.pipejukenerf", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Awake()
+        private void OnEnable()
         {
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            On.ShortcutHandler.Update += ShortcutHandler_Update;
+            //On.Tracker.ctor += Tracker_ctor;
+        }
+
+        /*private void Tracker_ctor(On.Tracker.orig_ctor orig, Tracker self, ArtificialIntelligence AI, int seeAroundCorners, int maxTrackedCreatures, int framesToRememberCreatures, float ghostSpeed, int ghostPush, int ghostPushSpeed, int ghostDismissalRange)
+        {
+            orig(self, AI, seeAroundCorners, maxTrackedCreatures, framesToRememberCreatures, ghostSpeed, ghostPush, ghostPushSpeed, ghostDismissalRange);
+            self.visualize = true;
+        }*/
+
+        private void ShortcutHandler_Update(On.ShortcutHandler.orig_Update orig, ShortcutHandler self)
+        {
+            orig(self);
+            for (int i = 0; i < self.transportVessels.Count; i++)
+            {
+                for (int j = 0; j < self.transportVessels.Count; j++)
+                {
+                    if (i != j && self.transportVessels[i].pos.FloatDist(self.transportVessels[j].pos) <= 1) // It is, in fact, two different creatures, and the creatures are next to each other in the short cut.
+                    {
+                        if (!(self.transportVessels[i].creature == null || self.transportVessels[i].creature.abstractCreature == null || self.transportVessels[i].creature.abstractCreature.abstractAI == null || self.transportVessels[i].creature.abstractCreature.abstractAI.RealAI == null || self.transportVessels[i].creature.abstractCreature.abstractAI.RealAI.tracker == null || self.transportVessels[j].creature == null || self.transportVessels[j].creature.abstractCreature == null))
+                        {
+                            try
+                            {
+                                self.transportVessels[i].creature.abstractCreature.abstractAI.RealAI.tracker.SeeCreature(self.transportVessels[j].creature.abstractCreature);
+                            }
+                            catch (System.Exception ex)
+                            {
+                                Debug.Log("Possible issue with PipeJukeNerf's plugin: " + ex.Message);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
